d4debaa0db6c73905e21c64abaa5334dadbfc290
skomius <skomantas.juskys@outlook.com>
2018-10-29 21:24:32 +0200

    cnavas

--- Universitity/Program.cs
+++ Universitity/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Universitity
+namespace Pure
 {
     public class Program
     {
