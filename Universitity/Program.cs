8af0ac291cefef894dd7db8376e768866bcba7db
skomius <skomantas.juskys@outlook.com>
2018-11-07 10:51:56 +0200

    darbas 2

--- Universitity/Program.cs
+++ Universitity/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Universitity
+namespace Experments
 {
     public class Program
     {
