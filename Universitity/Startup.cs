3ac74c712a6248787749dc04bd2d39e91767d26d
skomius <skomantas.juskys@outlook.com>
2019-01-06 22:18:19 +0200

    dbcontext

--- Universitity/Startup.cs
+++ Universitity/Startup.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Experments.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
 
 namespace Experments
 {
@@ -17,6 +20,10 @@ namespace Experments
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            var connection = @"Server=localhost\SQLEXPRESS;Database=Experments.Models;Trusted_Connection=True;ConnectRetryCount=0";
+
+            services.AddDbContext<BloggingContext>(options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
